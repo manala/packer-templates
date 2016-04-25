ff44640bd38d223940906c4b8f9cb4b7d1337f59
nervo <nervo@nervo.net>
2016-04-25 21:05:00 +0200

    Comment on openssh-server installation

--- scripts/openssh.sh
+++ scripts/openssh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# Openssh-server is de-facto installed for virtualbox provider,
+# nevermind, run installation anyway to simplify process
 apt-get -y install openssh-server
-#mkdir -p /var/run/sshd
