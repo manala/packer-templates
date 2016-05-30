91ab154069e4632b65c1ad857d783db24b68b864
Florian Rey <florian.rey@elao.com>
2016-05-30 09:15:23 +0200

    Keep bzip2 package installed for virtualbox

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -3,8 +3,11 @@
 # Remove standard virtualbox packages
 apt-get -y purge --auto-remove virtualbox-\*
 
+# Install dependencies
+DEBIAN_FRONTEND=noninteractive apt-get -y install bzip2
+
 # Install build dependencies & packages
-DEBIAN_FRONTEND=noninteractive apt-get -y install bzip2 dkms linux-headers-$(uname -r)
+DEBIAN_FRONTEND=noninteractive apt-get -y install dkms linux-headers-$(uname -r)
 
 # Install the virtualbox guest additions
 cd ~
@@ -14,4 +17,4 @@ umount /mnt
 rm VBoxGuestAdditions.iso
 
 # Remove build dependencies & packages
-apt-get -y purge --auto-remove bzip2 dkms linux-headers-$(uname -r)
+apt-get -y purge --auto-remove dkms linux-headers-$(uname -r)
