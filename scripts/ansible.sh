2faaa52ef14135b8fd7bfbca4d6b111be252d88b
Florian Rey <florian.rey@elao.com>
2016-05-31 16:13:15 +0200

    Always use apt-get --no-install-recommends option

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEBIAN_FRONTEND=noninteractive apt-get -y install ansible
+DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install ansible
 
 cat <<EOF > /etc/ansible/hosts
 localhost ansible_connection=local ansible_become=true
--- scripts/nfs.sh
+++ scripts/nfs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Install nfs & enable cache
-DEBIAN_FRONTEND=noninteractive apt-get -y install nfs-common cachefilesd
+DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install nfs-common cachefilesd
 sed -i 's/#RUN/RUN/' /etc/default/cachefilesd
