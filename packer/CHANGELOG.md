5c9c5be3b89a6b586d80b5d0ffa68e603d9ac7bb
Florian Rey <florian.rey@elao.com>
2018-03-26 10:58:22 +0200

    Introduce packer template

---
+++ packer/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+### Added
+- Packer 1.2.1
+- Debian 9.4.0
+- VirtualBox Guest Additions 5.2.8
---
+++ packer/packer/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get --quiet --verbose-versions --yes --no-install-recommends install curl ca-certificates bsdtar
+
+curl --location https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_amd64.zip \
+  | bsdtar -xf - -C /usr/local/bin
+
+chmod +x /usr/local/bin/packer
