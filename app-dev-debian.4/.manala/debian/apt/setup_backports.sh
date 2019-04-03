bbdfd0ae35febdf06c0523930ddf6008453c816b
Florian Rey <florian.rey@elao.com>
2019-04-03 14:41:37 +0200

    Typo

--- app-dev-debian.4/.manala/debian/apt/setup_backports.sh
+++ app-dev-debian.4/.manala/debian/apt/setup_backports.sh
@@ -11,6 +11,4 @@ eval "cat > /etc/apt/sources.list.d/deb_debian_org_debian.list << EOF
 deb http://deb.debian.org/debian ${RELEASE}-backports main
 EOF"
 
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 1394DEA3
-
 apt-get --quiet update
