7ac8b12e21a5ecdee6cd5304b8bfce913890c480
nervo <nervo@nervo.net>
2020-07-31 23:04:23 +0200

    Remove unused templates

--- packer/.manala/debian/sudo/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-printf "[\033[36mManala\033[0m] \033[32mDebian - Sudo - Install\033[0m\n"
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get --quiet --verbose-versions --yes --no-install-recommends install sudo
--- packer/.manala/system/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-printf "[\033[36mManala\033[0m] \033[32mSystem - Update\033[0m\n"
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get --quiet update
-
-apt-get --quiet --verbose-versions --yes --option Dpkg::Options::="--force-confold" --option Dpkg::Options::="--force-confdef" dist-upgrade
